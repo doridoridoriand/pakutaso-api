50942efdc62aea3770695b8a9fc807271bcf857d
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-04-29 00:06:08 +0900

    add databse config files

---
+++ config/schema.sql
@@ -0,0 +1,21 @@
+create table `contents` (
+  `id`      int(10) unsigned    not null,
+  `data_id` int(10)             not null,
+  `title`   varchar(255)        not null,
+  primary key(`id`),
+) engine=InnoDB default charset=utf8;
+
+create table `keywords` (
+  `id`      bigint(20) unsigned not null,
+  'data_id' int(10)             not null,
+  'keyword' varchar(255)        not null,
+  primary key(`id`),
+  index(`keyword`)
+) engine=InnoDB default charset=utf8;
+
+create table `images` (
+  `id`        int(10) unsigned  not null,
+  'data_id'   int(10)           not null,
+  'image_url' varchar(255)      not null,
+  primary key(`id`)
+) engine=InnoDB default charset=utf8;
