250fe2f7a627d92979df8caa9e9806900bb8143f
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-04-29 00:25:34 +0900

    fix tables

--- config/schema.sql
+++ config/schema.sql
@@ -7,15 +7,15 @@ create table `contents` (
 
 create table `keywords` (
   `id`      bigint(20) unsigned not null,
-  'data_id' int(10)             not null,
-  'keyword' varchar(255)        not null,
+  `data_id` bigint(20)          not null,
+  `keyword` varchar(2083)       not null,
   primary key(`id`),
   index(`keyword`)
 ) engine=InnoDB default charset=utf8;
 
 create table `images` (
   `id`        int(10) unsigned  not null,
-  'data_id'   int(10)           not null,
-  'image_url' varchar(255)      not null,
+  `data_id`   int(10)           not null,
+  `image_url` varchar(255)      not null,
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
